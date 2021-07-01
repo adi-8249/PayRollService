a01c0ace56bd97e8d2379b878753b34d4079fb12
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-01 22:58:23 +0530

    [Anjaneya]Added:Creating payroll service table

--- Payrollservice.sql
+++ Payrollservice.sql
@@ -2,3 +2,11 @@
 --UC1-Creating database and use
 create database payroll_service;
 Use payroll_service;
+
+--UC2-Creating Employee payrollservice Table
+create table EmployeePayRoll(
+EmployeeID int Identity(1,1) primary key,
+EmployeeName varchar(32),
+Salary float,
+StartDate date
+);
